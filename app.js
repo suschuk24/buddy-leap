fd402cb6a61f813a211878fdb121ddda8a9310ad
Seth Uschuk <suschuk24@gmail.com>
2021-02-17 12:31:49 -0600

    added platofrms to land on

--- app.js
+++ app.js
@@ -13,13 +13,26 @@ document.addEventListener('DOMContentLoaded', () => {
         doodler.style.bottom = doodlerBottomSpace + "px"
     }
     
-    
+    class Platform {
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement("div")
+
+            const visual = this.visual
+
+            visual.classList.add("platform")
+            visual.style.left = this.left + "px"
+            visual.style.bottom = this.bottom +"px"
+            grid.appendChild(visual)
+        }
+    }
 
     function createPlatforms() {
         for(let i = 0; i < platformCount; i++) {
             let platformSpacing = 600 / platformCount
             let newPlatformBottom = 100 + i * platformSpacing
-            let newPlatform = new Platform()
+            let newPlatform = new Platform(newPlatformBottom)
         }
     }
 
